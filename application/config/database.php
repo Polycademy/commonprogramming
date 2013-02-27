ba6a7d23a386148614aefcfaf3456f4b76d334d7
Roger Qiu <roger.qiu@polycademy.com>
2013-02-27 19:10:08 +1100

    Database test

--- application/config/database.php
+++ application/config/database.php
@@ -79,27 +79,58 @@
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => TRUE,
-	'db_debug' => TRUE,
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'autoinit' => TRUE,
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array()
-);
+if(ENVIRONMENT == 'development'){
+
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'commonprogramming',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => TRUE,
+		'db_debug' => TRUE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'autoinit' => TRUE,
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array()
+	);
+
+}elseif(ENVIRONMENT == 'cloud9'){
+
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'instance37562.db.xeround.com',
+		'username' => 'polycademy',
+		'password' => 'ergdegedrf',
+		'database' => 'polycademy',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => TRUE,
+		'db_debug' => TRUE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'autoinit' => TRUE,
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'port'	=> 5681,
+	);
+
+}elseif(ENVIRONMENT == 'production'){
+
+}
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
--- application/controllers/houston.php
+++ application/controllers/houston.php
@@ -2,17 +2,18 @@
 
 class Houston extends CI_Controller{
 
+	//always executed method
 	public function __construct(){
 		parent::__construct();
-		
-		$this->load->model('Houston_model');
-		
+		$this->load->database();
 	}
 
+	//default method
 	public function index(){
 		echo 'I\'m the index page!';
 	}
 	
+	//other method
 	public function ball(){
 	
 		$ball = $this->Houston_model->get_ball();
